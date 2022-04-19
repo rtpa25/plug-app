70b47dd1f090b7d6da26d8fcd3be511b5e34e024
rtpa25 <pandaronit25@gmail.com>
2022-04-19 17:47:52 +0530

    completed the resignin with anonymous feature

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,17 @@
 /** @format */
 
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signInWithPopup } from 'firebase/auth';
 import { auth, db, provider } from '../services/firebase';
-import { Button, Image } from 'antd';
+import { Button, Image, Modal } from 'antd';
 import { useAppDispatch } from '../hooks/index';
 import { setUserData } from '../store/slices/userSlice';
 import { DataSnapshot, onValue, ref, set } from 'firebase/database';
 import axios from 'axios';
 import { GoogleOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
+import localforage from 'localforage';
 
 const Container = styled.div`
   display: flex;
@@ -29,6 +30,19 @@ const Container = styled.div`
 const Login: FC = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
+  const [error, setError] = useState<any>();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
 
   const googleSigninHandler = async () => {
     try {
@@ -59,57 +73,88 @@ const Login: FC = () => {
         localStorage.setItem('uuid', data.uuid);
         navigate('/create-profile');
       });
-    } catch (error) {
-      console.error(error);
+    } catch (err) {
+      console.error(err);
+      setError(err);
+      showModal();
     }
   };
 
   const anonymosSigninHandler = async () => {
-    const res = await axios.get('https://randomuser.me/api/?results=1');
-    const displayName =
-      res.data.results[0].name.first + ' ' + res.data.results[0].name.last;
-    const email = res.data.results[0].email;
-    console.log(res.data.results[0].picture);
-    const photoURL = res.data.results[0].picture.large;
-    const uuid = res.data.results[0].login.uuid;
+    try {
+      const res = await axios.get('https://randomuser.me/api/?results=1');
+      const displayName =
+        res.data.results[0].name.first + ' ' + res.data.results[0].name.last;
+      const email = res.data.results[0].email;
+      console.log(res.data.results[0].picture);
+      const photoURL = res.data.results[0].picture.large;
+      const uuid = res.data.results[0].login.uuid;
 
-    set(ref(db, 'users/' + uuid), {
-      displayName: displayName,
-      email: email,
-      photoUrl: photoURL,
-      uuid: uuid,
-      status: '',
-      points: 0,
-      votes: {
-        asd: 1,
-      },
-      favourites: {
-        asd: 1,
-      },
-    }).catch((error) => {
-      console.error(error);
-    });
-
-    //save the data to the redux slice
-    dispatch(
-      setUserData({
-        userData: {
-          displayName: displayName as string,
-          email: email as string,
-          photoUrl: photoURL as string,
-          uuid: uuid,
-          status: '',
-          points: 0,
-          votes: {},
-          favourites: {},
+      set(ref(db, 'users/' + uuid), {
+        displayName: displayName,
+        email: email,
+        photoUrl: photoURL,
+        uuid: uuid,
+        status: '',
+        points: 0,
+        votes: {
+          asd: 1,
         },
-      })
-    );
+        favourites: {
+          asd: 1,
+        },
+      }).catch((error) => {
+        console.error(error);
+      });
 
-    localStorage.setItem('uuid', uuid);
+      //save the data to the redux slice
+      dispatch(
+        setUserData({
+          userData: {
+            displayName: displayName as string,
+            email: email as string,
+            photoUrl: photoURL as string,
+            uuid: uuid,
+            status: '',
+            points: 0,
+            votes: {},
+            favourites: {},
+          },
+        })
+      );
 
-    //navigate to the create profile page
-    navigate('/create-profile');
+      localStorage.setItem('uuid', uuid);
+      await localforage.setItem('anonym-uuid', uuid);
+
+      //navigate to the create profile page
+      navigate('/create-profile');
+    } catch (err) {
+      console.error(err);
+      setError(err);
+      showModal();
+    }
+  };
+
+  const reloginWithAnonymousProfile = async () => {
+    try {
+      const uuid = await localforage.getItem('anonym-uuid');
+      const userRef = ref(db, 'users/' + uuid);
+      onValue(userRef, (snapshot: DataSnapshot) => {
+        const data = snapshot.val();
+        if (data === null) {
+          setError('Please provide a valid username or rejoin');
+        } else {
+          dispatch(setUserData({ userData: data }));
+        }
+        localStorage.setItem('uuid', data.uuid);
+        navigate('/');
+      });
+      await localforage.setItem('anonym-uuid', uuid);
+    } catch (err) {
+      console.error(err);
+      setError(err);
+      showModal();
+    }
   };
 
   return (
@@ -132,6 +177,20 @@ const Login: FC = () => {
         size='large'>
         Stay Anonymos
       </Button>
+      <Button
+        type='primary'
+        shape='round'
+        style={{ width: '20rem', margin: '1rem' }}
+        onClick={reloginWithAnonymousProfile}
+        size='large'>
+        ReLogin With Anonymous Username
+      </Button>
+      <Modal
+        title={error}
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      />
     </Container>
   );
 };
